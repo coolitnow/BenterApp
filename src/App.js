fbbdea350e91e9a687ebe2393d4134bb29c0f1b6
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2021-11-25 18:28:57 -0500

    Add an API link and list Dune quotes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      duneQuotes: []
+    }
+
+  }
+
+  componentDidMount()
+  {
+    // this runs once this page has loaded
+    // this only runs once, unlike the constructor that runs twice, for whatever reason
+    this.loadDuneQuotes();
+  }
+
+  loadDuneQuotes() {
+    // go to that url
+    // get the results
+    // put them into this.state.movie_library
+
+    fetch("https://the-dune-api.herokuapp.com/quotes")
+      .then(res =>
+        res.json())
+      .then (json_response =>
+          this.setState({duneQuotes: json_response}));
+    
+  }
+  
+  render() {
+    let showQuote = [];
+    for(let i = 0; i < this.state.duneQuotes.length; i++)
+    {
+      showQuote.push(<li>{this.state.duneQuotes[i].quote}</li>)
+    }
+
+    return (
+      <div className="App">
+        Put quotes here
+        {showQuote}
+      </div>
+    );
+  }
 }
 
 export default App;
+
