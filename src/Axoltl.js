fd4b39e098b34f590fe8a708053e4a2c83d4d2d9
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2021-12-03 18:46:16 -0500

    Update Axolotl to use hooks and equip to throw error messages.

--- src/Axoltl.js
+++ src/Axoltl.js
@@ -1,48 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import DuneQ from "./DuneQ";
 
-class Axolotl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cutePic: ''
-        }
-    }
+function Axolotl() {
+    const [cutePic, setCutePic] = useState('')
+    const [isLoading, setIsLoading] = useState(true)
+    const [typeFetchError, setTypeFetchError] = useState(null)
 
-    componentDidMount()
-    {
-        this.loadAxos();
-
-    }
-
-    loadAxos() {
+    //similar to componentDidMount and componentDidUpdate"
+    useEffect(() => {
         //create cors-anywhere website via heroku in order to access axolotl api
         //put that address in front of the axolotlapi address
         //some APIs only allow real websites to access their info, so this is a way around that for testing and what not
         fetch("https://secure-brook-34239.herokuapp.com/https://axoltlapi.herokuapp.com/")
-        .then(res =>
-            res.json())
-        .then(json_response =>
-            this.setState({cutePic: json_response.message}));
-    }
+            .then(res =>
+                res.json())
+            .then(result => {
+                setCutePic(result.url)
+                setIsLoading(false)
+            },
+                (error) => {
+                    setTypeFetchError(true)
+                    setIsLoading(false)
+                }
+            )
+    }, [])
 
-    render(){
-         
-
-
-        return (
-            <div className="DuneQ-Container">
+    return (
+        <div className="DuneQ-Container">
 
             <div className="AxlContainer">
-            <img src={this.state.cutePic} alt="Axolotl Pic" className="AxlPic"/> 
-            <DuneQ />
+                <img src={cutePic} alt="Axolotl Pic" className="AxlPic" />
+                <DuneQ />
             </div>
-            </div>
-        )
-        
-    }
-    
+        </div>
+    )
+
 }
 
 export default Axolotl;
