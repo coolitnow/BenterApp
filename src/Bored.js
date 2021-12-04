8bf1af7cb80ce7b04f1027de98691af72f266dd6
Caitlin Bender <85584674+coolitnow@users.noreply.github.com>
2021-12-03 20:28:08 -0500

    Add error catch on Bore. Add hooks to bored.

--- src/Bored.js
+++ src/Bored.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-class Bored extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          boredStuff: {}
+function Bored() {
+  const [boredStuff, setBoredStuff] = useState({})
+  const [isLoading, setIsLoading] = useState(true)
+  const [typeFetchError, setTypeFetchError] = useState(null)
+
+  useEffect(() => {
+    fetch('http://www.boredapi.com/api/activity/')
+      .then(response => response.json())
+      .then(
+        (res) => {
+          setBoredStuff(res)
+          setIsLoading(false)
+        },
+        (error) => {
+          setTypeFetchError(true)
+          setIsLoading(false)
         }
-    }
-
-    componentDidMount () {
-        fetch('http://www.boredapi.com/api/activity/')
-        .then(response => response.json())
-        .then(boredStuff => 
-             {this.setState({boredStuff})} 
-             );
-    }
-
-    render() {
+      );
+  }, [])
 
 
-        return (
-          <div className="bored">
-            <div>Why Not:</div>
-            <div>{this.state.boredStuff.activity} </div>
-          </div>
-        );
-    }
+  return (
+    <div className="bored">
+      <div>Why Not:</div>
+      <div>{boredStuff.activity} </div>
+    </div>
+  );
+
 }
 
 export default Bored;
